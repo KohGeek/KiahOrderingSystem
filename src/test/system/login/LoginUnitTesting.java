cf132ac00016c48a033b109c93e87ee7ae10989e
Koh Jun Dong <KohComputer@gmail.com>
2021-04-01 10:03:09 +0800

    Initialising testing code

---
+++ src/test/system/login/LoginUnitTesting.java
@@ -0,0 +1,19 @@
+package system.login;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
+public class LoginUnitTesting {
+	
+	@Test
+	@Parameters
+	public void LoginMemberTests() {
+		
+	}
+	
+}
---
+++ src/test/system/menu/OrderUnitTesting.java
@@ -0,0 +1,19 @@
+package system.menu;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
+public class OrderUnitTesting {
+	
+	@Test
+	@Parameters
+	public void OrderTests() {
+		
+	}
+	
+}
