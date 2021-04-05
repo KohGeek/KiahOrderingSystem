a118a8a3bf5dff92a5ddd0ac448207af989310de
RickiE <rickie.leehao@gmail.com>
2021-04-05 13:03:07 +0800

    PaymentTest and PaymentClass

--- src/main/system/payment/Order.java
+++ src/main/system/payment/Order.java
@@ -117,6 +117,38 @@ class Cart {
 	}
 }
 
+class Payment {
+
+	private double totalPrice;
+	private PayStatus paymentStatus;
+	private PaymentMethod paymentMethod;
+
+	public Payment() {
+		this.totalPrice = 0;
+		this.paymentStatus = PayStatus.Unsuccessful;
+	}
+
+	public double getPaymentTotalPrice() {
+		return this.totalPrice;
+	}
+	
+	public void setPayStatus(PayStatus payStatus) {
+		this.paymentStatus = payStatus;
+	}
+	
+	public void setPaymentMethod(PaymentMethod paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	
+	public PayStatus getPaymentStatus() {
+		return this.paymentStatus;
+	}
+	
+	public PaymentMethod getPaymentMethod() {
+		return this.paymentMethod;
+	}
+}
+
 public class Order {
 
 	private int orderID;
