0bc9da5cdf6d961c3efa5e1a0e5cd0cc19d8721d
RickiE <rickie.leehao@gmail.com>
2021-04-08 14:37:05 +0800

    Update PaymentModuleTestSuite.java

--- src/test/system/domain/payment_module/PaymentModuleTestSuite.java
+++ src/test/system/domain/payment_module/PaymentModuleTestSuite.java
@@ -7,7 +7,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(value = Suite.class)
 @SuiteClasses(value = {
 	ProceedPaymentIntegrationTest.class,
-	ProceedPaymentUnitTest.class
+	ProceedPaymentUnitTest.class,
+	GetPaymentStatusUnitTest.class
 })
 
 public class PaymentModuleTestSuite {
